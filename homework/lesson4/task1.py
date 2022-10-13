61cfed86dff37683cb08bd5ad82faad3a3e1f1ff
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-10-13 20:12:28 +0200

    Create task1.py

---
+++ homework/lesson4/task1.py
@@ -0,0 +1,12 @@
+#Program that makes a random number between 1 and 10 and then you guess. If you are correct it will display a message and also when you are wrong
+#Also, it will put the number that was randomly selected so you see that the program doesn't cheat on you ;)
+
+from random import randint
+
+random_number = str(randint(1, 10))
+guess_number = input('Guess a number between 1 nad 10, NO DECIMALS: ')
+
+if random_number == guess_number:
+    print(f'Congrats! The number is {random_number}')
+else:
+    print(f'Wrong! The number was {random_number}.')
