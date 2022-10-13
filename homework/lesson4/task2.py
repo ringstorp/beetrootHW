01db7cfd0a2dacec23f8f79dd188c7d453134899
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-10-13 20:14:59 +0200

    Create task2.py

---
+++ homework/lesson4/task2.py
@@ -0,0 +1,7 @@
+#Program that takes your name and age and adds one year in a greeting!
+
+name = input("Write your name please: ")
+age = input("Write your age please: ")
+age = int(age) + 1
+
+print(f"Ahoy {name}, you are getting old! Your next birthday you will be {age} years old!")
