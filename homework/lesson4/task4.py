4d7ba47d9b2f0c02ffbeff7aa68da19259851cff
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-10-13 20:25:34 +0200

    Create task4.py

---
+++ homework/lesson4/task4.py
@@ -0,0 +1,7 @@
+#Math quiz program. Checks if you answer correctly according to the if-statement. Writes if you are correct or wrong.
+
+answer = input("3 * 3 = ")
+if answer == '9':
+    print("You are correct! Have a cookie :)")
+else:
+    print("Wrong! You should know this :(")
