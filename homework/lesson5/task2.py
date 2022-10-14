b630b2c2588ce6b2fd81ea1b1e701ae378088e31
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-10-14 15:20:52 +0200

    Update task2.py

--- homework/lesson5/task2.py
+++ homework/lesson5/task2.py
@@ -9,7 +9,7 @@ from random import sample
 
 list_1 = sample(range(1, 20), 11)
 list_2 = sample(range(1, 20), 11)
-print(f'list 1:\n{list_1}\n\nlist2:\n{list_2}\n')
+print(f"list 1:\n{list_1}\n\nlist2:\n{list_2}\n")
 
 x = 0
 my_set = set()
@@ -19,4 +19,4 @@ while x < len(list_1):
         my_set.add(list_1[x])
     x += 1
 
-print(f'The final common numbers are:\n{my_set}')
+print(f"The final common numbers are:\n{my_set}")
