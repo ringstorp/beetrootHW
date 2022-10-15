2186c3f40fdcabaa1e76ee3983afc49e1b6ec839
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-10-15 12:20:38 +0200

    Create task2.py

---
+++ homework/lesson6/task2.py
@@ -0,0 +1,24 @@
+#Create a function which takes as input two dicts with structure mentioned above,
+#then computes and returns the total price of stock.
+
+
+from collections import Counter
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+total = 0.0
+
+for product in stock:
+    total += prices[product] * stock[product]
+
+print(total)
