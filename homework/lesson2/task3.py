c1add65123f34f0730ffcb88f75842866e02a31c
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-12-20 19:14:43 +0100

    Create task3.py

---
+++ homework/lesson2/task3.py
@@ -0,0 +1,17 @@
+# Task 3
+
+# Using python as a calculator.
+
+# Make a program with 2 numbers saved in separate variables
+# a and b, then print the result for each.
+
+a = 28
+b = 6
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a % b)
+print(a // b)
