eaf797fabe80215f7138ab5a71fe2f6af5f7d057
ringstorp <84624984+ringstorp@users.noreply.github.com>
2022-12-20 19:14:09 +0100

    Create task1.py

---
+++ homework/lesson2/task1.py
@@ -0,0 +1,10 @@
+# Task 1
+# The greeting program.
+#
+# Make a program that has your name and the current day of
+# the week stored as separate variables and then prints a message.
+
+name = "Linus"
+day = "Wednesdays"
+
+print("Good day", name,'!', day, "are the perfect days to learn some Python.")
